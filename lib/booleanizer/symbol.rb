7f6f51191e30c826f57ae4a0282c4948e8b81f56
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-08-07 09:01:12 +0900

    `String#end_with?` を使うように

--- lib/booleanizer/symbol.rb
+++ lib/booleanizer/symbol.rb
@@ -1,6 +1,6 @@
 class Symbol
   def method_missing(method_name, *args, &block)
-    if method_name.to_s.match(/\?\Z/)
+    if method_name.to_s.end_with? "?"
       self == method_name.to_s[0..-2].to_sym
     else
       super
